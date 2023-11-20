25a55b3f74403c8ebdea61905a28449065e9a8e8
Pavan200509 <147812112+Pavan200509@users.noreply.github.com>
2023-11-20 10:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function callvalue(){
+    var n = document.getElementById('fullname').value;
+    var mid = document.getElementById('mailid').value;
+    var uq = document.getElementById('userquery').value;
+    document.writeln('Your Name :'+n);
+    document.writeln('Your mail :'+mid);
+    document.writeln('Your query:'+uq);
+}
+
